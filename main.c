a33ad5dad0f691c95d7f117b985d8fbfc39e32a2
Diego Anjos <33529467+DiegoSMAnjos@users.noreply.github.com>
2023-05-02 09:41:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+/*
+Integrantes:
+
+- Victor Rafael
+- Diego Anjos
+
+Descricao:
+
+Este jogo foi desenvolvido para aprovacao na disciplina de Sistemas Operacionais, semestre 2023.1, da Universidade Federal da Bahia.
+
+*/
+
+
+
+
 #include <stdio.h>
 
-int main(){return 0;}
+int main(){
+  
+  print("---------------------------\n");
+  print("--                       --\n");
+  print("--   JOGO DA FORCA       --\n");
+  print("--                       --\n");
+  print("---------------------------\n\n");
+  
+  
+  return 0;
+
+}
