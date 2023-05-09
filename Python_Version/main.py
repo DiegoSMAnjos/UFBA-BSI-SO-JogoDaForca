fe05f680a353d6437771f15203631576b2c24d4c
Victor <victormartinezrc@gmail.com>
2023-05-09 00:48:40 -0300

    Commit [Ajustes]

--- Python_Version/main.py
+++ Python_Version/main.py
@@ -1,6 +1,9 @@
 import random
 import threading
 from time import sleep
+import os
+
+
 
 MAX_TENTATIVAS = 6
 MAX_PALAVRA = 5
@@ -8,16 +11,12 @@ MAX_PALAVRA = 5
 palavra = ""
 dicaPalavra = ""
 tamanho = 0
-jogo_continua = True
+nomeVencedor = ''
 sem_leitura_palavra = threading.Semaphore(0)
 esperar_teclado_ser_liberado = threading.Semaphore(1)
 esperar_turno_validacao = threading.Semaphore(1)
+jogadores_ativos = 2
 
-
-# sem_leitura_letra = threading.Semaphore(0)
-# sem_atualiza_palavra = threading.Semaphore(1)
-# sem_atualiza_tentativas = threading.Semaphore(1)
-# esperarDefinicaoVariaveis = threading.Semaphore(1)
 
 
 class Jogador():
@@ -26,7 +25,10 @@ class Jogador():
         self.tentativas_restantes = MAX_TENTATIVAS
         self.adivinhacao = ""
         self.vitoria = False
+        self.jogo_continua = True
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def desenharArvore(tentativas):
     if tentativas >= 6:
@@ -78,7 +80,13 @@ def desenharArvore(tentativas):
               " |      / \\\n"
               "_|___")
     else:
-        pass  # não imprime nada se tentativas = 0
+        print("  _______\n"
+              " |/      |\n"
+              " |      (_)\n"
+              " |      /|\\\n"
+              " |       |\n"
+              " |      / \\\n"
+              "_|___")
 
 
 def definirPalavra():
@@ -102,76 +110,77 @@ def definirPalavra():
     ]
     escolhido = random.choice(db_palavras)
     dicaPalavra = escolhido[0]
-    palavra = escolhido[1]
+    palavra = escolhido[1].lower()
 
-    # palavra = input("ADM: insira a palavra para o jogo da forca (sem acentos): ")
     sem_leitura_palavra.release()
 
 
 def jogar(player: Jogador):
-    # esperarDefinicaoVariaveis.acquire()
-    # sem_leitura_palavra.acquire()
-    global palavra, tamanho, jogo_continua
+    global palavra, tamanho, nomeVencedor
 
     tamanho = len(palavra)
 
     player.adivinhacao = "_" * tamanho
-    # esperarDefinicaoVariaveis.release()
-    # sem_leitura_palavra.release()
-    while jogo_continua:
-        esperar_teclado_ser_liberado.acquire()
-        # sleep(1)
+    
+    while player.jogo_continua :
+        sleep(1)
+        esperar_turno_validacao.acquire()
 
-        # sem_atualiza_palavra.acquire()
+        if  (len(nomeVencedor) > 0):
+            break;
+       
         print(f"{player.name}, tente adivinhar a palavra (DICA: {dicaPalavra}): {player.adivinhacao}")
-        # sem_atualiza_palavra.release()
-
-        # sem_atualiza_tentativas.acquire()
+        
         print(f"Você tem {player.tentativas_restantes} tentativas restantes.")
-        # sem_atualiza_tentativas.release()
 
         letra = input("Digite uma letra: ")
-        letra = letra.lower()
-
-        esperar_teclado_ser_liberado.release()
-        esperar_turno_validacao.acquire()
+        
+        encontrou = False
         if letra in palavra:
             for i in range(tamanho):
-                if palavra[i] == letra:
+                if  palavra[i].lower() == letra.lower():
                     player.adivinhacao = player.adivinhacao[:i] + letra + player.adivinhacao[i + 1:]
+                    encontrou = True
+            if encontrou:
+                print(f"Letra correta!")
         else:
-            print(f"A letra '{letra}' não faz parte da palavra.\n")
+            print(f"A letra '{letra}' não faz parte da palavra.")
             player.tentativas_restantes -= 1
             if player.tentativas_restantes == 0:
-                jogo_continua = False
+                player.jogo_continua = False
         desenharArvore(player.tentativas_restantes)
-        if palavra == player.adivinhacao:
-            player.vitoria = True
-            jogo_continua = False
+        print(f"Palavra: {player.adivinhacao}")
+        if palavra.lower() == player.adivinhacao.lower():
+            nomeVencedor = player.name
+            player.jogo_continua = False
+        sleep(5)
+        clear_screen()
         esperar_turno_validacao.release()
 
 
 
 def main():
+    clear_screen()
     t1 = threading.Thread(target=definirPalavra)
-    player1 = threading.Thread(target=jogar, args=[Jogador('Luiz')])
-    player2 = threading.Thread(target=jogar, args=[Jogador('Victor')])
+    player_nome1 = input("Digite o nome do primeiro jogador: ")
+    player_nome2 = input("Digite o nome do segundo jogador: ")
+    player1 = threading.Thread(target=jogar, args=[Jogador(player_nome1)])
+    player2 = threading.Thread(target=jogar, args=[Jogador(player_nome2)])
 
     t1.start()
     t1.join()
+    clear_screen()
     player1.start()
     player2.start()
-
-    # sem_leitura_letra.acquire()
 
     player1.join()
     player2.join()
 
-    vitoria = False
-    if vitoria:
-        print(f"Parabéns, você venceu! A palavra era {palavra}.")
+
+    if len(nomeVencedor) > 0:
+        print(f"Parabéns {nomeVencedor}, você venceu! A palavra era {palavra}.")
     else:
-        print(f"Você perdeu! A palavra era {palavra}.")
+        print(f"Vocês perdeu! A palavra era {palavra}.")
 
 
 if __name__ == "__main__":
